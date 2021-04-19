77da7ed509552ae96d2fa9de05afc5122dab0f69
Anıl <anilakgunes@gmail.com>
2021-04-20 01:40:35 +0300

    moving git part2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './Theme'
+import Footer from "./Pages/Footer"
+import LandingPage from "./Pages/LandingPage"
+import NewsPage from "./Pages/NewsPage"
+import AuthPage from "./Pages/AuthPage"
+import {GlobalProvider} from "./components/shared/context/globalContext"
+import CoinListPage from "./Pages/CoinsListPage"
 
-function App() {
+const App = ()=> {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+        <GlobalProvider>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/features" component={()=> <div>Features</div>} />
+            <Route exact path="/coinmarkets" component={CoinListPage} />
+            <Route exact path="/news" component={NewsPage} />
+            <Route exact path="/blog" component={()=> <div>Blog</div>} />
+            <Route exact path="/contact" component={()=> <div>Contact</div>} />
+            <Route exact path="/auth" component={AuthPage} />
+        </GlobalProvider>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from "react-dom"
+import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+//----- Genel Notlar ----//: 
+
+// Grid :  grid sisteminde sadece containerlar justify & alignitems & direction problarını alabilir.
+
+// Container vs Item Container : They're technically the same in terms of the properties being applied, however, the 'container' prop causes that element to take up 100% of the width available to it. That's useful in some cases, but in other cases it can cause you to then not be able to align the items next to each other because they're all just taking up 100% width. So that's when you wrap the <Grid container> with the <Grid item> and then the container will only take up 100% width of that item, not the parent, and so you can then use the parent to align that <Grid item> normally
+
+// Typography: align probu ile texti sağa veya sola getirebiliyoruz.
+
+// Resim : Eğer resim çok dağılıyorsa resizelarda maxwidth ve minwidth kullanışlı olabilir.
+
+// Responsive: useMediaQuery ve ternary ile responsive css çok rahat yazılıyor. Ayrıca [theme.breakpoints.down('sm)] kullanarak da repsonsive css yazılabiliyor.
+
+// Itemları sağa ve sola ayırmak için justify mantıklı olabilir fakat, responsive gerektiği durumlarda auto-layout (sm, md) daha kullanışlı olabiliyor.
+
+// Route yaparken component koyduğumuz zaman props koyamıyoruz. Koymak istersek render propsu kullanmak zorundayız component yerine 77. videonun sonunda anlatımı mevcut.
+
+//IconButton , button ile aynı fakat resimler için customize edilmiş hali.
