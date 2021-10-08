3b83b84b9b03ca1b3c072dfec5bed3545a405d02
anlakngs <anilakgunes1@gmail.com>
2021-10-08 16:51:31 +0300

    fix enviroments var

--- src/fire.js
+++ src/fire.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';        // for authentication
+import 'firebase/auth'; // for authentication
 // import 'firebase/storage';     // for storage
 // import 'firebase/database';    // for realtime database
 // import 'firebase/firestore';   // for cloud firestore
@@ -8,19 +8,14 @@ import 'firebase/auth';        // for authentication
 
 // Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwAnaGJBdF9B5cfvu7qXp0N07PcRdbDxM",
-  authDomain: "crypto-tracker-9b91a.firebaseapp.com",
-  projectId: "crypto-tracker-9b91a",
-  storageBucket: "crypto-tracker-9b91a.appspot.com",
-  messagingSenderId: "43309707962",
-  appId: "1:43309707962:web:84d0d33d26b12d6f7c8883"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: 'crypto-tracker-9b91a.firebaseapp.com',
+  projectId: 'crypto-tracker-9b91a',
+  storageBucket: 'crypto-tracker-9b91a.appspot.com',
+  messagingSenderId: '43309707962',
+  appId: '1:43309707962:web:84d0d33d26b12d6f7c8883',
 };
 
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
-
-export default fire
-
-
-
-
+export default fire;
